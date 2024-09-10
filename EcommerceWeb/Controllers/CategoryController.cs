effb2da94f5763ea6c270a44f5eb0b66be91097d
Nupur Aidban <nupuraidban2002@gmail.com>
2024-09-10 21:32:09 +0530

    Added Category Model,Controller and related View

---
+++ EcommerceWeb/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using EcommerceWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceWeb.Models;
+
+namespace EcommerceWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var list = _db.Categories.ToList();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
